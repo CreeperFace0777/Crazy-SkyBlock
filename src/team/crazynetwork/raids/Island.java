ef48fb687f43f22611918833e78069dcda11a9c4
lutherwenxu <16671@kuencheng.edu.my>
2017-06-25 00:32:19 +0800

    Added some basic methods and private-ed some variables to prevent changing them. All privated variables can be access through methods.

--- src/team/crazynetwork/raids/Island.java
+++ src/team/crazynetwork/raids/Island.java
@@ -1,10 +1,31 @@
 package team.crazynetwork.raids;
 
+import java.util.Date;
+
 import org.bukkit.entity.Player;
 
 public class Island {
-    int x,y,z;
-    boolean raidable;
-    Player owner;
-
+    private int x,y,z; //The coordinates of the island
+    private Long raidableTime; //When it will be raidable
+    private Player owner; //The owner
+    
+    public Island(int x,int y,int z,Player owner){ //Constructor. Initializes everything from given vars.
+    	x=this.x;
+    	y=this.y;
+    	z=this.z;
+    	raidableTime=new Date().getTime() + (Long)SkyBlockRaids.getPlugin().getSettings("raidDelay"); //Gets current time and adds the delay to raid to it.
+    }
+    
+    public Player getOwner(){
+    	return owner; //Returns owner. Duh.
+    }
+    
+    public boolean isRaidable(){
+    	Long time = new Date().getTime(); //Get current time
+    	if (time >= raidableTime){ //Check if it is OK to raid.
+    		return true; //Returns true if it is.
+    	} else {
+    		return false; //Return false if it isn't.
+    	}
+    }
 }
--- src/team/crazynetwork/raids/SkyBlockRaids.java
+++ src/team/crazynetwork/raids/SkyBlockRaids.java
@@ -3,7 +3,25 @@ package team.crazynetwork.raids;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkyBlockRaids extends JavaPlugin {
-
-
-
+	private static SkyBlockRaids self;
+	
+	public void onEnable(){ //Runs on startup AND when server is reloaded. DO NOT assume 0 players.
+		self = this; //A workaround as static vars normally do not allow this.
+	}
+	
+	public void onDisable(){ //Save all the file here when the plugin is disabled.
+		
+	}
+	
+	public static SkyBlockRaids getPlugin(){ //Give other classes direct access to the plugin's methods.
+		return self;
+	}
+	
+	public Object getSettings(String settingName){ 
+		//getConfig conflicts with JavaPlugin.getConfig
+		//Returns a value from the config file.
+		
+		//To be implemented. A return of a placeholder for now.
+		return 1000;
+	}
 }
